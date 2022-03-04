fcce2c0b7d2c48445ed1070e7700ed3c2a34c341
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-03-04 11:45:21 +0530

    Create buildCode.groovy

---
+++ src/com/seh/util/buildCode.groovy
@@ -0,0 +1,21 @@
+package com.seh.util;
+
+def call(Map pipelineParams)
+{
+  if (pipelineParams.APPTYPE=="JAVA")
+  {
+  sh '''
+  echo "build the java code using maven"
+  cd $REPO
+  mvn clean package
+  '''
+  }
+  else if (pipelineParams.APPTYPE=="NODE")
+  {
+    sh '''
+    echo "build the node application using npm"
+    cd $REPO
+    npm start
+    '''
+  }
+}
