7a647ffba67b6e1c86f9037c832a92838036a01b
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-10-06 21:40:18 +0530

    Update staticCodeAnalysis.groovy

--- src/com/seh/util/staticCodeAnalysis.groovy
+++ src/com/seh/util/staticCodeAnalysis.groovy
@@ -5,6 +5,7 @@ def call(Map pipelineparams)
     echo "perform static code analysis using sonar"
     withSonarQubeEnv('SonarQubeScanner') {
         sh '''
+             cd $REPO_NAME/springmvc 
              mvn -version
              mvn sonar:sonar
          '''
