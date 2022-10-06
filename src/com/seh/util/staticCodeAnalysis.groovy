3fd1fb9cdc03bc76a72a9098d9eb188437a5429a
minaxijoshi3101 <minaxijoshi3101@gmail.com>
2022-10-06 21:27:53 +0530

    Update staticCodeAnalysis.groovy

--- src/com/seh/util/staticCodeAnalysis.groovy
+++ src/com/seh/util/staticCodeAnalysis.groovy
@@ -3,4 +3,9 @@ package com.seh.util
 def call(Map pipelineparams)
 {
     echo "perform static code analysis using sonar"
+    withSonarQubeEnv('SonarQubeScanner') {
+        sh '''
+             mvn sonar:sonar
+         '''
+   }
 }
